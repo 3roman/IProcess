e9d9c3d3f7d79099bc67716cde2abadd9a0a7c5d
guga <guga@gmail.com>
2022-12-30 13:18:16 +0800

    :pencil2:fix typos

--- IThermal/Condenser.cs
+++ IThermal/Condenser.cs
@@ -9,7 +9,7 @@ namespace IThermal
         public static double CondenserPressure(
             [ExcelArgument(Name = "tcw", Description = "cooling water temperature\n℃")] double tcw,
             [ExcelArgument(Name = "Δt1", Description = "cooling water temperature rise[≤10]\n℃")] double Δt1,
-            [ExcelArgument(Name = "Δt2", Description = "hot terminal temperature difference[≥2.8]\n℃")] double Δt2
+            [ExcelArgument(Name = "Δt2", Description = "terminal temperature difference[≥2.8]\n℃")] double Δt2
             )
         {
             double ts = tcw + Δt1 + Δt2;
@@ -21,7 +21,7 @@ namespace IThermal
         [ExcelFunction(Category = "IThermal_Condenser", Description = "solve for condensation water temperature\n℃")]
         public static double CondensationWaterTemperature(
             [ExcelArgument(Name = "pc", Description = "condenser pressure\nMPaA")] double pc,
-            [ExcelArgument(Name = "Δt", Description = "subcoold temperature of condensation water[≤3]\n℃")] double Δt
+            [ExcelArgument(Name = "Δt", Description = "condensate depression[≤3]\n℃")] double Δt
             )
         {
             return IAPWS97.P2T97(pc) - Δt;
